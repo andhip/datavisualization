76df53726c37bacf760dcf9e3ac9487c2fb33a68
andhip <andhi.puspianto10@gmail.com>
2021-03-26 19:07:34 +0700

    initial commit

---
+++ main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Visuallization</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script>
+    function refreshpage() {
+        location.reload();
+    }
+
+    function displayResult() {
+        var dimention=document.getElementById("dimention").value;
+        if (document.getElementById('qty').checked) {
+
+            $.ajax({
+                    
+            })
+        }
+    }
+     </script>
+</head>
+<body>
+    
+</body>
+</html>
