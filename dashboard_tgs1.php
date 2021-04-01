aa0cce57d89b68a93340db422ac2bf2baf11b8da
andhip <andhi.puspianto10@gmail.com>
2021-04-01 22:56:03 +0700

    u d1

--- dashboard_tgs1.php
+++ dashboard_tgs1.php
@@ -13,7 +13,7 @@
     include("koneksi.php");
     $dimensi = $_GET['dimensi'];
     $produk = mysqli_query($koneksi,"select * from fakta group by ".$dimensi."");
-    
+
     while($row = mysqli_fetch_array($produk)){
     $a[] = $row[''.$dimensi.''];
         
@@ -24,6 +24,38 @@
 
 
 ?>
+
+<div style="width: 800px;margin: 0px auto;">
+	<canvas id="barChart"></canvas>
+</div>
+
+<script>
+		var ctx = document.getElementById("barChart").getContext('2d');
+		var myChart = new Chart(ctx,config1);
+		var config1 = {
+			type: 'bar',
+			data: {
+				labels: <?php echo json_encode($a); ?>,
+				datasets: [{
+					label: 'Grafik qty Penjualan',
+					data: <?php echo json_encode($b); ?>,
+					backgroundColor: ['rgba(255, 99, 132, 0.2)','rgba(54, 162, 235, 0.2)'],
+					borderColor: 'rgba(255,99,132,1)',
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		};
+	</script>
+
     
 </body>
 </html>
